9bf1a606dffb995799402a032b107a326a1d3e82
Bushaija <Bushaijarobert@gmail.com>
2023-06-23 21:44:52 +0200

    Finalized Caesar Cipher encryption
    Completed the implementation of Caesar Cipher encryption and ready for deployment.

--- ceasarCipherRightShift/cearsarEncoding.py
+++ ceasarCipherRightShift/cearsarEncoding.py
@@ -7,3 +7,10 @@ def encrypt(text,s):
       else:
          result += chr((ord(char) + s - 97) % 26 + 97)
    return result
+
+text = "CEASER CIPHER DEMO"
+s = 4
+
+print("Plain Text: " + text)
+print("Shift pattern: " + str(s))
+print("Cipher: " + encrypt(text,s))
