f514b72c03b7091ac91ae7d9273be1c40e43cc19
Bushaija <Bushaijarobert@gmail.com>
2023-06-23 21:58:12 +0200

    Finalized linear search implementation
    Completed the implementation of linear search functionality and ready for deployment.

--- linearSearch/linearSearchAlgo.py
+++ linearSearch/linearSearchAlgo.py
@@ -8,3 +8,8 @@ arr = [2, 3, 5, 7, 9, 1, 4, 6, 8, 10]
 max_size = len(arr)
 key = 8
 result = iterative_linear(arr, max_size - 1, key)
+
+if result != -1:
+    print("The element", key, "is found at index", result, "and in position", result + 1)
+else:
+    print("The element", key, "is not present in the given array")
