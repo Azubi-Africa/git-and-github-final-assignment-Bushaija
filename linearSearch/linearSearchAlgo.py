6dae747cc2c8fcc0159a36ec2c75be728798b703
Bushaija <Bushaijarobert@gmail.com>
2023-06-23 21:56:18 +0200

    Displayed search result
    Displayed the result of the linear search operation, indicating whether the key element was found or not.

--- linearSearch/linearSearchAlgo.py
+++ linearSearch/linearSearchAlgo.py
@@ -3,3 +3,8 @@ def iterative_linear(arr, n, key_element):
         if arr[x] == key_element:
             return x
     return -1
+
+arr = [2, 3, 5, 7, 9, 1, 4, 6, 8, 10]
+max_size = len(arr)
+key = 8
+result = iterative_linear(arr, max_size - 1, key)
